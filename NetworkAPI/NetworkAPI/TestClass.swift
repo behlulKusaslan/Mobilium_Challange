dfa7ef0f1a27354a06a12d0257183f7ea6e80778
behlul <behlulkusaslan@gmail.com>
2018-12-20 11:02:06 +0300

    Network framework linked in to project

---
+++ NetworkAPI/NetworkAPI/TestClass.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass.swift
+//  NetworkAPI
+//
+//  Created by behlul on 20.12.2018.
+//  Copyright © 2018 behlul. All rights reserved.
+//
+
+import Foundation
+
+public final class TestClass {
+    
+    public static func testRun() {
+        print("/--- Test runs! ---/")
+    }
+}
